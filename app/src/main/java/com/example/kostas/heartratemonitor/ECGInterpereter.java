553ea44cc2efd6d2dc4931373bb40ed78dd5c6f2
ileri <celil.ileri@bil.omu.edu.tr>
2019-05-06 00:52:00 +0300

    add signal spiltter and processer

--- app/src/main/java/com/example/kostas/heartratemonitor/ECGInterpereter.java
+++ app/src/main/java/com/example/kostas/heartratemonitor/ECGInterpereter.java
@@ -1,36 +1,108 @@
 package com.example.kostas.heartratemonitor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import org.apache.commons.collections4.queue.CircularFifoQueue;
 
 public class ECGInterpereter {
-    public final int MAX_QUEUE_SIZE = 1000;
-    private Queue<Integer> values;
+    //public final int MAX_QUEUE_SIZE = 1000;
+    //private Queue<Integer> values;
     private Integer[] valuesArray;
+    private Integer[] waveArray;
 
     ECGInterpereter(){
-        values = new CircularFifoQueue<Integer>(MAX_QUEUE_SIZE);
+        //values = new CircularFifoQueue<>(MAX_QUEUE_SIZE);
     }
+
+    /*
+    // ACTIVATE IN NEED OF REALTIME SIGNAL STREAMING
 
     public void addValue(int value){
         values.add(value);
     }
 
     public void addValues(int[] values){
-        for(int value : values){
+        for(int value : values)
             this.values.add(value);
-        }
     }
 
     public void addValues(String valuesString){
         String[] stringValueArray = valuesString.split(" ");
-        for(String strValue : stringValueArray){
+        for(String strValue : stringValueArray)
             addValue(Integer.valueOf(strValue));
-        }
     }
 
-    public int[] getPQRST(){
-        valuesArray = values.toArray(valuesArray);
+    public void resetValues(){
+        values.clear();
+    }
+    */
+
+    private void addValues(String valuesString){
+        String[] stringValueArray = valuesString.split(" ");
+        valuesArray = new Integer[stringValueArray.length];
+        for(int i = 0; i < stringValueArray.length; i++)
+            valuesArray[i] = Integer.valueOf(stringValueArray[i]);
+    }
+
+    private int[] splitECGSignal(String signal){
+        addValues(signal);
+        return splitECGSignal();
+    }
+
+    private int[] splitECGSignal(){
+        List<Integer> rWavesIndexes = new ArrayList<>();
+
+        boolean isRWave = false;
+        int rStartIndex = 0, rStopIndex = 0;
+
+        for(int i = 0; i < valuesArray.length; i++){
+            int value = valuesArray[i];
+            if(!isRWave){
+                if(value > 650){
+                    /* R wave started */
+                    isRWave = true;
+                    rStartIndex = i;
+                }else{
+                    /* R wave has not started */}
+            }else{
+                if(value > 650){
+                    /* R wave continues */ }
+                else{
+                    /* R wave finished */
+                    rStopIndex = i;
+                    rWavesIndexes.add(((rStartIndex + rStopIndex) / 2));
+                    isRWave = false;
+                }
+            }
+        }
+
+        Integer[] rWavesIndexesArray = new Integer[rWavesIndexes.size()];
+        rWavesIndexesArray = rWavesIndexes.toArray(rWavesIndexesArray);
+
+        int[] wavesEndIndexes = new int[rWavesIndexesArray.length];
+
+        int i = 0;
+        for(; i < rWavesIndexesArray.length-1; i++)
+            wavesEndIndexes[i] = rWavesIndexesArray[i] + (int)((rWavesIndexesArray[i+1] - rWavesIndexesArray[i])*(0.66));
+
+        wavesEndIndexes[i] = valuesArray.length;
+
+        return wavesEndIndexes;
+    }
+
+    public String processECGSignal(String signal){
+        int[] endTimes = splitECGSignal(signal);
+        int startTime = 0;
+        for(int i = 0; i < endTimes.length; i++){
+            System.arraycopy(valuesArray, startTime, waveArray, 0, endTimes[i]-startTime);
+            getPQRST();
+        }
+        return "TODO : Add Information to this String";
+    }
+
+    private int[] getPQRST(){
+        //valuesArray = values.toArray(valuesArray);
         int indexR = findR();
         int indexQ = findQ(indexR);
         int indexP = findP(indexQ);
@@ -40,7 +112,7 @@ public class ECGInterpereter {
     }
 
     private int findR(){
-        return getMaxInRange(0, valuesArray.length);
+        return getMaxInRange(0, waveArray.length);
     }
 
     private int findQ(int indexR){
@@ -52,18 +124,18 @@ public class ECGInterpereter {
     }
 
     private int findS(int indexR){
-        return getMinInRange(indexR, valuesArray.length);
+        return getMinInRange(indexR, waveArray.length);
     }
 
     private int findT(int indexS){
-        return getMaxInRange(indexS, valuesArray.length);
+        return getMaxInRange(indexS, waveArray.length);
     }
 
     private int getMaxInRange(int startIndex, int endIndex){
         int maxValue = 0, maxIndex = 0;
         for(int i = startIndex; i < endIndex; i++){
-            if(valuesArray[i] > maxValue){
-                maxValue = valuesArray[i];
+            if(waveArray[i] > maxValue){
+                maxValue = waveArray[i];
                 maxIndex = i;
             }
         }
@@ -73,11 +145,12 @@ public class ECGInterpereter {
     private int getMinInRange(int startIndex, int endIndex){
         int minValue = 1023, minIndex = 0;
         for(int i = startIndex; i < endIndex; i++){
-            if(valuesArray[i] < minValue){
-                minValue = valuesArray[i];
+            if(waveArray[i] < minValue){
+                minValue = waveArray[i];
                 minIndex = i;
             }
         }
         return minIndex;
     }
+
 }
